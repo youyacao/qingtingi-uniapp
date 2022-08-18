6b60140f9f1233c6d708d0b42d432fa8482c9007
Redkider <792124443@qq.com>
2022-08-18 22:55:25 +0800

    202208

--- App.vue
+++ App.vue
@@ -2,24 +2,31 @@
 export default {
 	onLaunch: function() {
 		console.log('App Launch');
-		this.$http.get('/config?key=im').then(res=>{
-			
-			let tabBarOptions = {
-				index: 3, //要修改的下标
-				text: res.data.im_menu_name, //文字
-				iconPath: res.data.im_menu_icon,
-				selectedIconPath: res.data.im_menu_icon_ing
-			};
-			uni.setTabBarItem(tabBarOptions);
-		})
+		
 		
 		/*  */
 	},
 	onShow: function() {
 		console.log('App Show');
+		uni.$emit('setTabBarItem')
+		
 	},
 	onHide: function() {
 		console.log('App Hide');
+	},
+	created() {
+		uni.$on('setTabBarItem', res => {
+			this.$http.get('/config?key=im').then(res=>{
+				
+				let tabBarOptions = {
+					index: 3, //要修改的下标
+					text: res.data.im_menu_name, //文字
+					iconPath: res.data.im_menu_icon,
+					selectedIconPath: res.data.im_menu_icon_ing
+				};
+				uni.setTabBarItem(tabBarOptions);
+			})
+		})
 	}
 };
 </script>
--- main.js
+++ main.js
@@ -5,9 +5,49 @@ import Vue from 'vue'
 import GoEasy from './lib/goeasy-2.4.7.min';
 Vue.config.productionTip = false
 import api from '@/components/http/index.js'
-
     // 全局挂载后使用
 Vue.prototype.$http = api
+Vue.prototype.$http.get('/config?key=im').then(res=>{
+	
+	const goEasy = GoEasy.getInstance({
+		host:'hangzhou.goeasy.io',//应用所在的区域地址: 【hangzhou.goeasy.io |singapore.goeasy.io】
+		appkey:res.data.im_secretkey,//'BC-08885949b4f54edfaa9ee4a7a786c51a',	// common key,
+	    modules:['im','pubsub'],
+		// true表示支持通知栏提醒，false则表示不需要通知栏提醒
+	    allowNotification: true //仅有效于app,小程序和H5将会被自动忽略
+	});
+	
+	goEasy.onClickNotification((message) => {
+		let currentUrl;
+		const routes = getCurrentPages();
+	
+		if ( routes &&  routes.length ) {
+			const curRoute = routes[routes.length - 1].route;
+			const curParam = routes[routes.length - 1].options;
+			currentUrl = '/' + curRoute + `?to=${curParam.to}`;
+		}
+	
+	    let newUrl;
+	    switch(message.toType) {
+	        case GoEasy.IM_SCENE.PRIVATE:
+	            newUrl = '/pages/chat/privateChat/privateChat?to=' + message.senderId;
+	            break;
+	        case GoEasy.IM_SCENE.GROUP:
+	            newUrl = '/pages/chat/groupChat/groupChat?to=' + message.groupId;
+	            break;
+	    }
+	
+	    if (currentUrl !== newUrl) {
+	        uni.navigateTo({
+	            url: newUrl,
+	        });
+	    }
+	
+	});
+	
+	Vue.prototype.GoEasy = GoEasy;
+	Vue.prototype.goEasy = goEasy;
+})
 App.mpType = 'app'
 const app = new Vue({
     ...App
@@ -26,44 +66,7 @@ export function createApp() {
 // #endif
 import wLoading from "@/components//w-loading/w-loading.vue";
 Vue.component('w-loading',wLoading)
-const goEasy = GoEasy.getInstance({
-	host:'hangzhou.goeasy.io',//应用所在的区域地址: 【hangzhou.goeasy.io |singapore.goeasy.io】
-	appkey:'BC-08885949b4f54edfaa9ee4a7a786c51a',	// common key,
-    modules:['im','pubsub'],
-	// true表示支持通知栏提醒，false则表示不需要通知栏提醒
-    allowNotification: true //仅有效于app,小程序和H5将会被自动忽略
-});
 
-goEasy.onClickNotification((message) => {
-	let currentUrl;
-	const routes = getCurrentPages();
-
-	if ( routes &&  routes.length ) {
-		const curRoute = routes[routes.length - 1].route;
-		const curParam = routes[routes.length - 1].options;
-		currentUrl = '/' + curRoute + `?to=${curParam.to}`;
-	}
-
-    let newUrl;
-    switch(message.toType) {
-        case GoEasy.IM_SCENE.PRIVATE:
-            newUrl = '/pages/chat/privateChat/privateChat?to=' + message.senderId;
-            break;
-        case GoEasy.IM_SCENE.GROUP:
-            newUrl = '/pages/chat/groupChat/groupChat?to=' + message.groupId;
-            break;
-    }
-
-    if (currentUrl !== newUrl) {
-        uni.navigateTo({
-            url: newUrl,
-        });
-    }
-
-});
-
-Vue.prototype.GoEasy = GoEasy;
-Vue.prototype.goEasy = goEasy;
 
 Vue.prototype.formatDate = function (t) {
     t = t || Date.now();
